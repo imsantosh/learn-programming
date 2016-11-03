628351304b6cbab6de518953a290503f9b438929
santosh <santosh76792@gmail.com>
2016-11-03 20:37:41 +0530

    Header modified and new paragraph added.

--- index.php
+++ index.php
@@ -5,7 +5,15 @@
 </head>
 <body>
 <div id="container">
-	<h1>Welcome to Learning Programming</h1>
+	<h1>Welcome to Learning Programming !</h1>
+
+	<p>
+		You are downloading the latest (2.10.2) 64-bit version of Git for Windows. This is the most recent maintained build. It was released about 19 hours ago, on 2016-11-02.
+
+If your download hasn't started, click here to download manually.
+
+Other Git for Windows downloads
+	</p>
 </div>
 </body>
 </html>
